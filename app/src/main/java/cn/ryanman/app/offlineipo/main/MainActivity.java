1525862a75e429548e4577c4f43bf6ed8b964020
ryan <ryan.hanyj@gmail.com>
2017-01-09 14:39:40 +0800

    update

--- app/src/main/java/cn/ryanman/app/offlineipo/main/MainActivity.java
+++ app/src/main/java/cn/ryanman/app/offlineipo/main/MainActivity.java
@@ -1,29 +1,20 @@
 package cn.ryanman.app.offlineipo.main;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
 
 import com.ashokvarma.bottomnavigation.BottomNavigationBar;
 import com.ashokvarma.bottomnavigation.BottomNavigationItem;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import cn.ryanman.app.offlineipo.R;
-import cn.ryanman.app.offlineipo.utils.WebUtils;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView webText;
     private BottomNavigationBar bottomNavigationBar;
     private Fragment[] fragments;
-    //private IpoTodayFragment ipoTodayFragment;
-    //private IpoListFragment ipoListFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +23,10 @@ public class MainActivity extends AppCompatActivity {
         setViews();
         setDefaultFragment();
 
-        //IpoListAsyncTask task = new IpoListAsyncTask();
-        //task.execute();
     }
 
     private void setViews(){
-        //webText = (TextView) findViewById(R.id.webtext);
+
         bottomNavigationBar = (BottomNavigationBar) findViewById(R.id.bottom_navigation_bar);
 
         bottomNavigationBar
@@ -75,24 +64,6 @@ public class MainActivity extends AppCompatActivity {
         ft.add(R.id.fragment, fragments[0]);
         ft.commit();
 
-    }
-
-    private class IpoListAsyncTask extends AsyncTask<Void, Integer, String> {
-
-        @Override
-        protected String doInBackground(Void... params) {
-            try {
-                return WebUtils.getIpoItems().size() + "";
-            } catch (Exception e) {
-                return null;
-            }
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            if (result != null)
-                webText.setText(result);
-        }
     }
 
 }
